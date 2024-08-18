69f148a545526bab601990af989b376e1d8d4198
thanakirtN <thanakrit.n@codemonday.com>
2024-08-19 04:45:44 +0700

    problem01

---
+++ BossBabysRevenge.ts
@@ -0,0 +1,26 @@
+const bossBabysRevenge = (s: string) => {
+    let shot: number = 0; // count the time that Boss Baby was shot
+
+    if (s.startsWith("R")) return "Bad Boy"; // case : Boss Baby initiate shot first
+
+    if (s.endsWith("S")) return "Bad Boy";
+    // This case means Boss Baby didn't revenge the last shot
+    // Then output is Bad Boy
+    // And we don't need to execute the loop
+
+    for (const events of s) {
+        if (events === "S") {
+            shot++; // increase the count that Boss Baby must shot back
+        } else if (events === "R") {
+            if (shot > 0) shot--;
+            // one shot have been revenged
+
+            // if shot <= 0 means Boss Baby shoots extra shot
+            // we don't have to do anything in this case
+        } else return "Bad Boy";
+        // handle the case Input contains characters other than S and R
+        // Include s and r
+    }
+
+    return shot === 0 ? "Good Boy" : "Bad Boy"; // shot is 0 means all shots have been revenged
+};
